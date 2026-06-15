f4e189fcc6e06d6b055b8cf1672d9d4e80685710
kiki <378031327@qq.com>
2026-06-15 23:15:42 +0800

    优化 mysql 配置

--- config/mysql.php
+++ config/mysql.php
@@ -10,8 +10,8 @@ return [
     'resources' => [
 
         'local' => [
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'database' => 'default',
             'username' => 'default',
             'password' => 'password',
